731e048201ba5d7fa09827f2f91aa28e6885ab00
Bernard <bernardleongije@gmail.com>
2021-01-25 17:41:20 +0800

    fix: add customer email

--- routes/routes.js
+++ routes/routes.js
@@ -365,7 +365,7 @@ const AlarmRoutes = (app) =>{
         // to: ['emily.koh@izeem.com','sandee@izeem.com','bernard.leong@izeem.com','pohkiat@ze.com.sg','marcuschen@ze.com.sg','durai@ze.com.sg','shawnlee@ze.com.sg','thomas@ze.com.sg','jeromeang@ze.com.sg','geraldina.koh@sembcorp.com'],
         const data = {
             from: 'bernard.pub125147@gmail.com',
-            to: ['emily.koh@izeem.com','bernardleongqijie@gmail.com','seahyw@gmail.com'],
+            to: ['emily.koh@izeem.com','sandee@izeem.com','bernard.leong@izeem.com','pohkiat@ze.com.sg','marcuschen@ze.com.sg','durai@ze.com.sg','shawnlee@ze.com.sg','thomas@ze.com.sg','jeromeang@ze.com.sg','geraldina.koh@sembcorp.com','seahyw@gmail.com'],
             subject: 'Alarm Trigger',
             html: template
         };
