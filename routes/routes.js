c3af611d6d72986550913bb9c4c29b6dbf6cfc69
Bernard <bernardleongije@gmail.com>
2021-02-08 15:56:03 +0800

    fix: add back customers

--- routes/routes.js
+++ routes/routes.js
@@ -564,7 +564,7 @@ const AlarmRoutes = (app) =>{
         // to: ['emily.koh@izeem.com','bernardleongqijie@gmail.com','pohkiat@ze.com.sg','marcuschen@ze.com.sg','durai@ze.com.sg','shawnlee@ze.com.sg','thomas@ze.com.sg','jeromeang@ze.com.sg','geraldina.koh@sembcorp.com', ''],
         const data = {
             from: 'bernard.pub125147@gmail.com',
-            to: ['emily.koh@izeem.com','bernardleongqijie@gmail.com', 'seahyw@gmail.com'],
+            to: ['emily.koh@izeem.com','bernardleongqijie@gmail.com','pohkiat@ze.com.sg','marcuschen@ze.com.sg','durai@ze.com.sg','shawnlee@ze.com.sg','thomas@ze.com.sg','jeromeang@ze.com.sg','geraldina.koh@sembcorp.com','seahyw@gmail.com'],
             subject: 'One Alarm Trigger Received',
             html: template
         };
