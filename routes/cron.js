5c21a2f3ed38fffc87ecc814df2453cb7542c5d2
Bernard <bernardleongije@gmail.com>
2021-03-23 12:02:07 +0800

    fix: cron run every 30 minutes

--- routes/cron.js
+++ routes/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 var AWS = require("aws-sdk");
 
 const scheduleCron = (cron, compactor) => {
-    cron.schedule('* * * * *', async()=> {
+    cron.schedule('0,30 * * * *', async()=> {
         var saveWeightCollected = await compactor.saveWeightCollected()
         console.log(saveWeightCollected)
     })
